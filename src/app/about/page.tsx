4a4e5cbba454d59d0456c7346bf46edc25fe22ae
Devasya Patel <devasyaspatel@gmail.com>
2025-12-14 02:06:21 +0530

    fixed deployment error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,17 +5,11 @@ import Navbar from '@/components/Navbar';
 import AboutPage from '@/components/AboutPage';
 
 export default function Page() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-        if (localStorage.getItem('user_id')) {
-            setIsLoggedIn(true);
-        }
-    }, []);
+    // Auth handled by Navbar internally
 
     return (
         <div className="min-h-screen bg-gray-50">
-            <Navbar isLoggedIn={isLoggedIn} />
+            <Navbar />
             {/* Add padding-top to account for fixed Navbar */}
             <div className="pt-16">
                 <AboutPage />
